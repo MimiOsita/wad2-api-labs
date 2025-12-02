5bddacc4530d2cf6d33cf930498fbe6fd3ea5ec5
MimiOsita <20097663@mail.wit.ie>
2025-12-02 17:22:30 +0000

    Authentication front-end set up

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -2,6 +2,10 @@ import dotenv from 'dotenv';
 import express from 'express';
 import tasksRouter from './api/tasks';
 import './db';
+// other imports
+import cors from 'cors';
+
+
 
 dotenv.config();
 
@@ -16,6 +20,9 @@ const errHandler = (err, req, res, next) => {
 
 
 const app = express();
+// Enable CORS for all requests
+app.use(cors());
+
 
 const port = process.env.PORT;
 
